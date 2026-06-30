227c7dad388ee08df91a14e3eeae5e7c3291c7d3
SakshiK2503 <sakshikesarwani2503@gmail.com>
2026-07-01 00:22:53 +0530

    Point frontend at new Vercel backend

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL: (window.location.hostname==="localhost" ? 
      "http://localhost:5000/api" :
-     "https://teamsync-production-592e.up.railway.app/api" )
+     "https://team-sync-w7bc.vercel.app/api" )
 });
 
 API.interceptors.request.use((req) => {
